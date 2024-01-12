2a804a92778fa304146f28a39eb7dbce63a59404
Evan Anderson <evanmand02@gmail.com>
2024-01-12 11:38:24 -0700

    testing still

--- evans-shell-script.sh
+++ evans-shell-script.sh
@@ -1,3 +1,4 @@
-libcamera-jpeg -o /home/cpl/Documents/images/test2.jpg
+# libcamera-jpeg -o /home/cpl/Documents/images/test2.jpg
 # libcamera-jpeg -o /home/cpl/Documents/images/`\%m_\%d_\%Y_\%H:\%M:\%S`.jpg
+libcamera-jpeg -o /home/cpl/Documents/images/$(date +"%FT%H%M").jpg
 echo test
