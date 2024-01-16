f91248c3b3384fb62e98620adf2c91a84bc8409b
Evan Anderson <evanmand02@gmail.com>
2024-01-16 15:11:54 -0700

    Setting fuzz to 50

--- evan-main.py
+++ evan-main.py
@@ -3,4 +3,4 @@
 from sys import argv
 from lib import gpix
 
-gpix._analyzeImage_('/home/cpl/Documents/images/' + argv[1], False, 0).writeCsv('/home/cpl/Documents/pixel-analysis-logging')
+gpix._analyzeImage_('/home/cpl/Documents/images/' + argv[1], False, 50).writeCsv('/home/cpl/Documents/pixel-analysis-logging')
