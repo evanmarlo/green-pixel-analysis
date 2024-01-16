5e41594eb6ec72340193c581cd3d0b2d13ae92c1
Evan Anderson <evanmand02@gmail.com>
2024-01-16 14:55:54 -0700

    Adding code to push CSV results to Git repo

--- evans-shell-script.sh
+++ evans-shell-script.sh
@@ -2,5 +2,11 @@
 mkdir -p /home/cpl/Documents/images
 libcamera-jpeg -o /home/cpl/Documents/images/$(date +"%m_%d_%Y_%H:%M").jpg
 
-# Feed image into python program
+# Feed image into python program, writing CSV file
 python3 evan-main.py $(date +"%m_%d_%Y_%H:%M").jpg
+
+# Push CSV file to Git repo
+cd /home/cpl/Documents/pixel-analysis-logging
+git add --all
+git commit -m "Automated push from $hostname"
+git push
