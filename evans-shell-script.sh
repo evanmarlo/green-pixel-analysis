9b927422e9001b8a3452b296e33da86c49b84280
Evan Anderson <evanmand02@gmail.com>
2024-01-16 14:34:27 -0700

    Updateing shell script

--- evans-shell-script.sh
+++ evans-shell-script.sh
@@ -3,3 +3,4 @@ mkdir -p /home/cpl/Documents/images
 libcamera-jpeg -o /home/cpl/Documents/images/$(date +"%m_%d_%Y_%H:%M").jpg
 
 # Feed image into python program
+python3 evan-main.py $(date +"%m_%d_%Y_%H:%M").jpg
